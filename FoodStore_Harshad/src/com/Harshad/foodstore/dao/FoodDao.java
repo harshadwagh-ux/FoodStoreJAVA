8ee55d2bb38301f27f079c811d229fa0da6286d1
harshadwagh-ux <harshadwagh535@gmail.com>
2024-12-27 14:41:08 +0530

    Add files via upload
    Food store application console base project

---
+++ FoodStore_Harshad/src/com/Harshad/foodstore/dao/FoodDao.java
@@ -0,0 +1,21 @@
+package com.shashank.foodstore.dao;
+
+import java.util.List;
+
+import com.Harshad.foodstore.pojo.Food;
+
+public interface FoodDao 
+{
+	boolean add(Food food);
+	/*
+	 	here boolean to single that respected Food is Add or not 
+	 	if added then return true otherwise return false.
+	*/
+	boolean update(int foodId,Food food);
+	boolean delete(int foodId);
+	List<Food> all();
+	Food getById(int foodId);
+	
+}
+// All these method are abstract ie. it empty method
+// to implement them we another class which name FoodDaoImpl
